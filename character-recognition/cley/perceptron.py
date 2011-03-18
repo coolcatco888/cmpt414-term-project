331d015b4f1d2322479ebdcc3aac258780af110b
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-03-18 02:02:48 +0000

    Finished basic perceptron

--- character-recognition/cley/perceptron.py
+++ character-recognition/cley/perceptron.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plot
 
 class Perceptron:
 
-    x = [][]    # inputs
-    theta = 0.0 # threshold
-    maxTime     # max time steps
-    w = [][]    # weights
-    y = []      # outputs
-    d = []      # desired outputs
-    n = 0.1     # gain factor
+    x = []              # inputs
+    theta = 0.0         # threshold
+    maxTime = 1000      # max time steps
+    w = []              # weights
+    y = []              # outputs
+    d = []              # desired outputs
+    n = 0.1             # gain factor
 
     # Steps 1 and 2
     def __init__(self, x, theta, maxTime, d, n):
-        self.x.append(x)
+        self.x = x
         self.theta = theta
         self.maxTime = maxTime
         self.d = d # Step 2
@@ -22,36 +22,44 @@ class Perceptron:
         self.w.append([random() for i in range(len(x))]) # Step 1
         
     # Hardlimiter Function
-    def fh(alpha):
+    def fh(self, alpha):
         retval = 1
-        if(alpha < 0)
+        if(alpha < 0):
             retval = -1
         return retval
 
     # Step 3
-    def calculateOutput(t):
+    def calculate_output(self, t):
         summation = 0
         for i in range(len(x)):
-            summation += self.w[t][i] * self.x[t][i] - self.theta
-        y.append(self.fh(summation))
+            summation += self.w[t][i] * self.x[i] - self.theta
+        self.y.append(self.fh(summation))
 
     # Step 4
-    def adaptWeights(t):
-    weights = []
+    def adapt_weights(self, t):
+        weights = []
         for i in range(len(x)):
-            weights.append(self.w[t][i] + self.n * (self.d[t] - self.y[t]) * self.x[t][i])
-    w.append(weights)
+            weights.append(self.w[t][i] + self.n * (self.d[t] - self.y[t]) * self.x[i])
+            if self.x[i] > self.y[t]:
+                self.d.append(-1.0)
+            else:
+                self.d.append(1.0)
+        self.w.append(weights)
 
-    def finalOutput():
+
+    def final_output(self):
         for t in range(maxTime):
-            self.calculateOutput(t)
-            self.adaptWeights(t)
-        return y
+            self.calculate_output(t)
+            self.adapt_weights(t)
+        return self.y
 
 if __name__ == "__main__":
-    x = [random() for i in range(3)]
+    x = [uniform(-10.0, 10.0) for i in range(1)]
     theta = 0.1
-    maxTime = 1000
+    maxTime = 100
     d = [1]
     n = 0.15
     p = Perceptron(x, theta, maxTime, d, n)
+    y = p.final_output()
+    print "y = ", y[maxTime - 1]
+    print "end"
