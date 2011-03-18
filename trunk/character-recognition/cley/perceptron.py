071850fd8afcce675b5cf34c90357f8e0b9f7a33
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-03-18 01:14:45 +0000

    need to test

--- trunk/character-recognition/cley/perceptron.py
+++ trunk/character-recognition/cley/perceptron.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plot
 class Perceptron:
 
     x = [][]    # inputs
-    kLayers = 1 # number of layers
     theta = 0.0 # threshold
     maxTime     # max time steps
     w = [][]    # weights
@@ -14,14 +13,13 @@ class Perceptron:
     n = 0.1     # gain factor
 
     # Steps 1 and 2
-    def __init__(self, x, theta, kLayers, maxTime, d, n):
-        self.x[0] = x
+    def __init__(self, x, theta, maxTime, d, n):
+        self.x.append(x)
         self.theta = theta
-        self.kLayers = kLayers
         self.maxTime = maxTime
         self.d = d # Step 2
         self.n = n
-        self.w[0] = [random() for i in range(len(x))] # Step 1
+        self.w.append([random() for i in range(len(x))]) # Step 1
         
     # Hardlimiter Function
     def fh(alpha):
@@ -31,20 +29,29 @@ class Perceptron:
         return retval
 
     # Step 3
-    def calculateOutput():
+    def calculateOutput(t):
         summation = 0
         for i in range(len(x)):
-            summation += w[t][i] * x[t][i] - theta
-        y[t] = fh(summation)
+            summation += self.w[t][i] * self.x[t][i] - self.theta
+        y.append(self.fh(summation))
 
     # Step 4
-    def adaptWeights():
+    def adaptWeights(t):
+    weights = []
         for i in range(len(x)):
-            w[t + 1][i] = w[t][i] + n * (d[t] - y[t]) * x[t][i]
+            weights.append(self.w[t][i] + self.n * (self.d[t] - self.y[t]) * self.x[t][i])
+    w.append(weights)
 
     def finalOutput():
         for t in range(maxTime):
-            self.calculateOutput()
-            self.adaptWeights()
+            self.calculateOutput(t)
+            self.adaptWeights(t)
         return y
 
+if __name__ == "__main__":
+    x = [random() for i in range(3)]
+    theta = 0.1
+    maxTime = 1000
+    d = [1]
+    n = 0.15
+    p = Perceptron(x, theta, maxTime, d, n)
