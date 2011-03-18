7acb1bb1631fddea92ce2b94216c53b5c42d7391
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-03-18 18:40:02 +0000

    added my own ocr network implementation

---
+++ trunk/character-recognition/cley/ocr_network.py
@@ -0,0 +1,22 @@
+from perceptron import Perceptron
+
+__author__="cley"
+__date__ ="$Mar 18, 2011 11:24:22 AM$"
+
+class OCRNetwork:
+
+    self.image_library = []
+
+    def __init__(self, image_library):
+        self.image_library = image_library
+
+    # train system using back propagation
+    def train(self):
+        return
+
+    # Run to test image for matchin
+    # Returns index to image in library
+    def test(self, image):
+        return
+if __name__ == "__main__":
+    print "Hello World"
