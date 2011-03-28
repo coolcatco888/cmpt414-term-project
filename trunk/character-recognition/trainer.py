072399a97e9b34a9c8aa132deb07d965f8253fc6
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-03-28 21:04:56 +0000

    added methods to trainer

--- trunk/character-recognition/trainer.py
+++ trunk/character-recognition/trainer.py
@@ -4,11 +4,20 @@ __author__="cley"
 __date__ ="$Mar 28, 2011 11:42:30 AM$"
 
 class Trainer:
-    
+
+    training_data = []
+    network = 0
+    max_steps = 10000
+
     def __init__(self, training_data):
-        """Documentation"""
+        self.training_data = training_data
+        self.network = Network(4,[6, 4])
 
+    def train(self):
+        return
 
+    def test(self):
+        return
 
 if __name__ == "__main__":
     print "Hello World"
