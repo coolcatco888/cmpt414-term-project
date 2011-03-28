92f893860a1fd97e4c2a1af0b840067bf50a533b
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-03-28 20:51:19 +0000

    added trainer file

---
+++ character-recognition/trainer.py
@@ -0,0 +1,14 @@
+from network import Network
+
+__author__="cley"
+__date__ ="$Mar 28, 2011 11:42:30 AM$"
+
+class Trainer:
+    
+    def __init__(self, training_data):
+        """Documentation"""
+
+
+
+if __name__ == "__main__":
+    print "Hello World"
