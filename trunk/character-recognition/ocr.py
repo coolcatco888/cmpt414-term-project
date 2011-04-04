6c4d0e344778975ecaed5ca2f01a246e4e5de03c
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-04 07:35:36 +0000

    Added ocr

---
+++ trunk/character-recognition/ocr.py
@@ -0,0 +1,16 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+
+class ocr:
+    training_images = []
+
+    def __init__(self, training_images):
+        self.training_images = training_images
+
+    def train(self):
+        return
+
+    def test(self):
+        return
+    
