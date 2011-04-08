d575c2ea682dd01f389160a447b910661739e8e2
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-08 04:06:50 +0000

    added some test images

--- character-recognition/ocr.py
+++ character-recognition/ocr.py
@@ -1,17 +1,30 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+import Magick
 
 
 class ocr:
+    training_image_names = []
     training_images = []
 
     def __init__(self, training_images):
-        self.training_images = training_images
-        
+        self.training_image_names = training_image_names
+        self.__process_images_for_network()
 
+        
+    def __process_images_for_network(self):
+        for image_name in self.training_image_names:
+            img = Magick.read(image_name)
+            self.training_images.append(img)
+        return
+    
     def train(self):
         return
 
     def test(self):
         return
-    
+
+if __name__ == "__main__":
+
+    training_images = []
+    training_images.append("")
+
+    return
