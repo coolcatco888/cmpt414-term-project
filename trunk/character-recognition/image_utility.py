9c000308e9a8603422dfa826ed34643a4289bcb0
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-09 05:26:29 +0000

    added get color

--- trunk/character-recognition/image_utility.py
+++ trunk/character-recognition/image_utility.py
@@ -14,9 +14,25 @@ def set_color(image, x, y, r, g, b):
 
     color = "#" + r_hex + g_hex + b_hex
 
-    print color
     image.pixelColor(x, y, color);
 
+# image - PythonMagickImage
+# x - x coordinate of pixel
+# y - y coordinate of pixel
+#
+# Returns
+# r - r channel 0-255
+# g - g channel 0-255
+# b - b channel 0-255
+def get_color(image, x, y):
+
+    color = image.pixelColor(x, y)
+
+    r = int((float(color.redQuantum()) / 65536.0) * 256.0)
+    g = int((float(color.greenQuantum()) / 65536.0) * 256.0)
+    b = int((float(color.blueQuantum()) / 65536.0) * 256.0)
+
+    return [r, g, b]
 
 def format_hex_number(hex_number):
     number = str(hex(hex_number))
