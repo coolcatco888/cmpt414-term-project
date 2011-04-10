3b8ee1cc23245538ab1a3d92f4fbde81323bc88b
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-10 01:32:08 +0000

    added about dialog

--- trunk/character-recognition/main_program.py
+++ trunk/character-recognition/main_program.py
@@ -7,12 +7,31 @@ import gtk
 
 class  MainProgramGTK:
 
+    builder = 0
+
     def __init__(self):
+
+        # Load GUI from glade xml
         filename = "main_menu.glade"
         builder = gtk.Builder()
         builder.add_from_file(filename)
-        signals = { "on_window_destroy" : gtk.main_quit }
+
+        # Bind functions to GUI
+        signals = {
+            "on_window_destroy" : gtk.main_quit,
+            "on_about_show" : self.about_show
+        }
         builder.connect_signals(signals)
+        self.builder = builder
+
+
+    def about_show(self, widget):
+        self.window = self.builder.get_object("aboutdialog")
+	response = self.window.run()
+
+        if response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
+            self.window.hide()
+
 
 if __name__ == "__main__":
     app = MainProgramGTK()
