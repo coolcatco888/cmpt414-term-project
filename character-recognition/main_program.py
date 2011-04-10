4aa79d6968127cc1908f772c6bf81433d01c8829
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-10 03:51:52 +0000

    file loading from image now complete

--- character-recognition/main_program.py
+++ character-recognition/main_program.py
@@ -4,6 +4,7 @@ __author__="cley"
 __date__ ="$9-Apr-2011 5:07:40 PM$"
 
 import gtk
+import PythonMagick
 
 TEXT_LOAD = "Load"
 TEXT_SAVE = "Save"
@@ -11,6 +12,8 @@ TEXT_SAVE = "Save"
 class  MainProgramGTK:
 
     builder = 0
+    image_file = 0
+    network_file = 0
 
     def __init__(self):
 
@@ -23,7 +26,7 @@ class  MainProgramGTK:
         signals = {
             "on_window_destroy" : gtk.main_quit,
             "on_about_show" : self.about_show,
-            "on_load_network" : self.load_network
+            "on_load_save_dialog_show" : self.load_save_dialog_show
         }
         builder.connect_signals(signals)
         self.builder = builder
@@ -39,14 +42,37 @@ class  MainProgramGTK:
         if response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
             self.window.hide()
 
-    def load_network(self, widget):
+    def load_save_dialog_show(self, widget):
+        label = widget.get_label()
+
+        button = self.builder.get_object("loadSaveButton")
+        button.set_label(label)
+        
         self.window = self.builder.get_object("filechooserdialog")
-        self.window.set_title("Choose Neural Network...")
+        self.window.set_title(label + "...")
         response = self.window.run()
 
-        if response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
-            self.window.hide()
+        dialog = self.window
 
+        if response == gtk.RESPONSE_OK:
+            if label[:4] == "Load":
+                if label[5:] == "Image":
+                    self.__display_image(dialog.get_filename(), "testingImage")
+                    self.window.hide()
+                elif label[5:] == "Network":
+                    self.network_file = dialog.get_filename()
+
+            elif label[4:] == "Save":
+                    """"""
+        elif response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
+            self.window.hide()
+    def __display_image(self, image_file, object_name):
+        image = PythonMagick.Image(image_file)
+        image.scale("100x100")
+        image.write("test_image.png")
+
+        testing_image_display = self.builder.get_object(object_name)
+        testing_image_display.set_from_file("test_image.png")
 
 if __name__ == "__main__":
     app = MainProgramGTK()
