7ed500cc3bd739aaeb50c9edf46cd3d84173738c
coolcatco888 <coolcatco888@d1a19cf0-8b45-60c9-1819-d1a1648520cf>
2011-04-10 00:29:31 +0000

    Constructing GUI

---
+++ character-recognition/main_program.py
@@ -0,0 +1,18 @@
+# Main entry point into program
+
+__author__="cley"
+__date__ ="$9-Apr-2011 5:07:40 PM$"
+
+import gtk
+
+class  MainProgramGTK:
+
+    def __init__(self):
+        filename = "main_menu.glade"
+        builder = gtk.Builder()
+        builder.add_from_file(filename)
+        builder.connect_signals(self)
+
+if __name__ == "__main__":
+    app = MainProgramGTK()
+    gtk.main()
